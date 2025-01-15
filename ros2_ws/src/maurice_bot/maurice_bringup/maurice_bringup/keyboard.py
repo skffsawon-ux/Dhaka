2c292b3072489eb04c9ea97a70cfd1df48622101
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-01-15 12:16:16 -0800

    keybopard better

--- ros2_ws/src/maurice_bot/maurice_bringup/maurice_bringup/keyboard.py
+++ ros2_ws/src/maurice_bot/maurice_bringup/maurice_bringup/keyboard.py
@@ -3,6 +3,8 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from pynput import keyboard
+import threading
 
 class KeyboardController(Node):
     def __init__(self):
@@ -19,8 +21,12 @@ class KeyboardController(Node):
         # Publisher for velocity commands
         self.twist_pub = self.create_publisher(Twist, '/cmd_vel', 10)
         
-        # Create timer for reading keyboard input
+        # Create timer for publishing commands
         self.create_timer(0.1, self.timer_callback)
+        
+        # Set up keyboard listener
+        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.listener.start()
         
         self.get_logger().info('Keyboard Controller initialized')
         self.get_logger().info("""
@@ -33,35 +39,37 @@ Control Instructions:
     q: quit
         """)
     
-    def timer_callback(self):
+    def on_press(self, key):
         try:
-            key = input().lower().strip()
+            key_char = key.char.lower()
             
             # Update speed and turn rate based on key presses
-            if key == 'w':  # increase speed
+            if key_char == 'w':
                 self.current_speed = min(self.current_speed + self.speed_increment, self.max_speed)
-            elif key == 'x':  # decrease speed
+            elif key_char == 'x':
                 self.current_speed = max(self.current_speed - self.speed_increment, -self.max_speed)
-            elif key == 'a':  # turn left
+            elif key_char == 'a':
                 self.current_turn = min(self.current_turn + self.turn_increment, self.max_turn)
-            elif key == 'd':  # turn right
+            elif key_char == 'd':
                 self.current_turn = max(self.current_turn - self.turn_increment, -self.max_turn)
-            elif key == 's':  # stop everything
+            elif key_char == 's':
                 self.current_speed = 0.0
                 self.current_turn = 0.0
-            elif key == 'q':  # quit
+            elif key_char == 'q':
                 self.destroy_node()
                 rclpy.shutdown()
-                return
-            
-            # Create and publish Twist message
-            msg = Twist()
-            msg.linear.x = self.current_speed
-            msg.angular.z = self.current_turn
-            self.twist_pub.publish(msg)
-            
-        except EOFError:
+                return False
+        except AttributeError:
             pass
+        
+        return True
+    
+    def timer_callback(self):
+        # Create and publish Twist message
+        msg = Twist()
+        msg.linear.x = self.current_speed
+        msg.angular.z = self.current_turn
+        self.twist_pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
