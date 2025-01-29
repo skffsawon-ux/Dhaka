b4a3058e705db1426c436916e518dd09f7bce24a
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-01-29 00:03:41 -0800

    cirrect params

--- ros2_ws/src/maurice_bot/maurice_control/launch/joystick.launch.py
+++ ros2_ws/src/maurice_bot/maurice_control/launch/joystick.launch.py
@@ -7,10 +7,10 @@ import os
 
 def generate_launch_description():
     # Get the package share directory
-    pkg_dir = get_package_share_directory('maurice_bringup')
+    pkg_dir = get_package_share_directory('maurice_control')
     
     # Path to the config file
-    config_file = os.path.join(pkg_dir, 'config', 'robot_config.yaml')
+    config_file = os.path.join(pkg_dir, 'config', 'motion_control.yaml')
     
     # Create the joystick node
     joystick_node = Node(
