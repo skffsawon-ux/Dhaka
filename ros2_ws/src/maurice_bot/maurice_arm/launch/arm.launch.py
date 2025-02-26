6ee36b5d2858e6f503128e85f41f513ef543da67
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-02-26 12:32:23 -0800

    camer alunahc file

--- ros2_ws/src/maurice_bot/maurice_arm/launch/arm.launch.py
+++ ros2_ws/src/maurice_bot/maurice_arm/launch/arm.launch.py
@@ -21,6 +21,21 @@ def generate_launch_description():
         output='screen'
     )
 
+    # Create the camera node
+    camera_node = Node(
+        package='v4l2_camera',
+        executable='v4l2_camera_node',
+        name='v4l2_camera',
+        parameters=[{
+            'device': '/dev/video0',
+            'width': 640,
+            'height': 480,
+            'fps': 30
+        }],
+        output='screen'
+    )
+
     return LaunchDescription([
-        maurice_arm_node
+        maurice_arm_node,
+        camera_node
     ])
