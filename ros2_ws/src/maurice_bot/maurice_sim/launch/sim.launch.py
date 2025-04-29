475a45ec3f0ab966e02df911a69c5c764f6a90e6
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-04-29 03:28:35 -0700

    fixed urdf and sim

--- ros2_ws/src/maurice_bot/maurice_sim/launch/sim.launch.py
+++ ros2_ws/src/maurice_bot/maurice_sim/launch/sim.launch.py
@@ -13,9 +13,9 @@ def generate_launch_description():
             parameters=[{'robot_description': open(urdf_file).read()}]
         ),
         Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
             output='screen'
-        )
+        ),
     ])
