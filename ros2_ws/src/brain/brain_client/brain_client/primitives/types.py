2bad35a943ae8501d09779c642473b21875c3197
Axel Peytavin <peytavin@stanford.edu>
2025-05-26 19:40:16 -0700

    New primitive methods to allow to get state fromthe robot

--- ros2_ws/src/brain/brain_client/brain_client/primitives/types.py
+++ ros2_ws/src/brain/brain_client/brain_client/primitives/types.py
@@ -12,6 +12,17 @@ class PrimitiveResult(Enum):
     CANCELLED = "cancelled"  # The primitive was cancelled before completion
 
 
+class RobotStateType(Enum):
+    """
+    Enum representing the types of robot state a primitive might require.
+    """
+
+    LAST_IMAGE_B64 = "last_image_b64"
+    LAST_ODOM = "last_odom"
+    LAST_MAP = "last_map"
+    # Add more states as needed, e.g., LAST_DEPTH_IMAGE_B64
+
+
 class Primitive(ABC):
     @property
     @abstractmethod
@@ -45,6 +56,22 @@ class Primitive(ABC):
         Returns a message describing the cancellation result.
         """
         pass
+
+    def update_robot_state(self, **kwargs):
+        """
+        Update the primitive with the latest robot state.
+        Subclasses can override this to store relevant data.
+        Example: self.last_image = kwargs.get('last_image_b64')
+        """
+        pass
+
+    def get_required_robot_states(self) -> list[RobotStateType]:
+        """
+        Declare the robot states required by this primitive.
+        Subclasses should override this to specify their needs.
+        Returns a list of RobotStateType enum members.
+        """
+        return []
 
     def guidelines(self):
         """
