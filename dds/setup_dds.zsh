cae505c490d45c39426749523c1d6e8b55fd17d3
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2025-12-26 18:46:56 -0800

    enable zenoh shared memory

--- dds/setup_dds.zsh
+++ dds/setup_dds.zsh
@@ -2,3 +2,9 @@
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 export ROS_DOMAIN_ID=0
 export ZENOH_ROUTER_CHECK_ATTEMPTS=0 # wait forever for router start
+
+# enabled shared memory transport w/ 12 MiB buffer per node
+# TODO: once we move nodes to be individually launchable, this value 
+# should be set on a per-node basis to avoid wasting 12MiB per node 
+# (or potentially bottlenecking the camera nodes)
+export ZENOH_CONFIG_OVERRIDE='transport/shared_memory/enabled=true;transport/shared_memory/transport_optimization/pool_size=12582912'
