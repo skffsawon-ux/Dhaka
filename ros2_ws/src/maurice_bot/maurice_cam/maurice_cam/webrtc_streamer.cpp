3e925bbcec9e317e1dbc6b8f66c1e61221387bb5
vignesh anand <vignesh@innate.bot>
2026-01-12 23:35:02 -0800

    vp8 :(

--- ros2_ws/src/maurice_bot/maurice_cam/maurice_cam/webrtc_streamer.cpp
+++ ros2_ws/src/maurice_bot/maurice_cam/maurice_cam/webrtc_streamer.cpp
@@ -313,20 +313,18 @@ void WebRTCStreamer::on_start(const std_msgs::msg::String::SharedPtr msg)
     "caps=video/x-raw,format=BGR,width=640,height=480,framerate=30/1 ! "
     "queue leaky=downstream max-size-buffers=1 max-size-time=0 max-size-bytes=0 ! "
     "videoconvert ! "
-    "x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 bitrate=5000 ! "
-    "video/x-h264,profile=constrained-baseline ! "
-    "rtph264pay config-interval=-1 pt=96 ! "
-    "application/x-rtp,media=video,encoding-name=H264,clock-rate=90000,payload=96 ! "
+    "vp8enc deadline=1 error-resilient=partitions keyframe-max-dist=30 ! "
+    "rtpvp8pay pt=96 ! "
+    "application/x-rtp,media=video,encoding-name=VP8,clock-rate=90000,payload=96 ! "
     "webrtc.sink_0 "
     
     "appsrc name=src_arm is-live=true format=time "
     "caps=video/x-raw,format=BGR,width=640,height=480,framerate=15/1 ! "
     "queue leaky=downstream max-size-buffers=1 max-size-time=0 max-size-bytes=0 ! "
     "videoconvert ! "
-    "x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 bitrate=2500 ! "
-    "video/x-h264,profile=constrained-baseline ! "
-    "rtph264pay config-interval=-1 pt=97 ! "
-    "application/x-rtp,media=video,encoding-name=H264,clock-rate=90000,payload=97 ! "
+    "vp8enc deadline=1 error-resilient=partitions keyframe-max-dist=30 ! "
+    "rtpvp8pay pt=97 ! "
+    "application/x-rtp,media=video,encoding-name=VP8,clock-rate=90000,payload=97 ! "
     "webrtc.sink_1";
 
   GError* error = nullptr;
